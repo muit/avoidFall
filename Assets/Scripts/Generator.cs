096f37bf5684f0b01dfacf15bca5f221276016cb
muit <miguel_3c@hotmail.com>
2015-01-27 21:55:48 +0100

    Now Rendering Terrain

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,21 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Generator : MonoBehaviour {
 	private SimplexNoise noise = null;
+	private RectangleRange range;
+	private Vector2 pre_position;
+	private Vector2 center;
+	private TerrainCache tc = new TerrainCache ();
 
 	public int rangeHeight = 10;
 	public int rangeWidth = 5;
 	public string seed = "sweetcandy";
-	
-	private Vector2 pre_position;
-	public Vector2 center;
 	public GameObject player;
-	private RectangleRange range;
-
+	public GameObject cubePrefab;
 	//Noise Attributes
 	public int   octaves     = 1;
-	public float multiplier  = 0.1f;
+	public float frequency  = 0.1f;
 	public float amplitude   = 1;
 	public float lacunarity  = 1;
 	public float persistence = 1;
@@ -24,8 +25,14 @@ public class Generator : MonoBehaviour {
 
 	void Start () {
 		noise = new SimplexNoise (seed);
-		noise.setup (octaves, multiplier, amplitude, high, low, lacunarity, persistence);
-		range = new RectangleRange (this, rangeHeight, rangeWidth);
+		noise.setup (octaves, frequency, amplitude, high, low, lacunarity, persistence);
+		range = new RectangleRange (this);
+
+		center = new Vector2 (
+			Mathf.Round (player.transform.position.x), 
+			Mathf.Round (player.transform.position.z)
+		);
+		range.Update(center);
 	}
 
 	void Update () {
@@ -41,13 +48,17 @@ public class Generator : MonoBehaviour {
 	}
 
 	void NewCube(Vector2 position){
-		//if(cubes[position.x][position.y] != null)
+		if (tc.Get (position) == null) {
 			float y = noise.getCoherent (position.x, 0, position.y);
-			Debug.Log (new Vector3 (position.x, y, position.y));
+			GameObject go = Instantiate(cubePrefab, new Vector3 (position.x, y, position.y), Quaternion.identity) as GameObject;
+			go.transform.parent = transform;
+			go.SetActive(true);
+			tc.Set(position, go);
+		}
 	}
 
 	public class Range {
-		private Generator generator;
+		protected Generator generator;
 
 		public Range(Generator generator){
 			this.generator = generator;
@@ -57,19 +68,32 @@ public class Generator : MonoBehaviour {
 	}
 
 	public class RectangleRange : Range{
-		public int height;
-		public int width;
-
-		public RectangleRange(Generator generator, int height, int width) : base(generator){
-			this.height = height;
-			this.width = width;
+		public RectangleRange(Generator generator) : base(generator){
 		}
 
 		public override void Update(Vector2 center){
-			Debug.Log ("Rendering new Blocks...");
+			for (int x = (int)center.x-generator.rangeWidth; x<=(int)center.x+generator.rangeWidth; x++){
+				for (int y = (int)center.y-generator.rangeHeight; y<=(int)center.y+generator.rangeHeight; y++){
+					generator.NewCube(new Vector2(x,y));
+				}
+			}
+		}
+	}
 
-
-
+	public class TerrainCache: Dictionary<string, GameObject>  {
+		public GameObject Get(Vector2 pos) {
+			try{
+				return this [pos.x + ":" + pos.y];
+			}catch(KeyNotFoundException){
+				return null;
+			}
+		}
+		public void Set(Vector2 pos, GameObject go) {
+			go.name = pos.x + ":" + pos.y;
+			this.Add (go.name, go);
+		}
+		public void Replace(Vector2 pos, GameObject go) {
+			this [pos.x + ":" + pos.y] = go;
 		}
 	}
 }
